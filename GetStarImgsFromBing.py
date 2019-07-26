e9ccd1165b172797a70dce34a060b6e548e93d56
freehawkzk <freehawkzk@126.com>
2019-07-26 13:37:57 +0800

    0.1.5.4
    1、通过百度爬取明星名字
    2、通过bing爬取明星图片

---
+++ GetStarImgsFromBing.py
@@ -0,0 +1,14 @@
+import os
+from icrawler.builtin import BingImageCrawler
+path = r'D:\Work\XiongDi\ImageDataset\asianStars'
+f = open('starName.txt', 'r')
+lines = f.readlines()
+for i, line in enumerate(lines):
+    name = line.strip('\n')
+    file_path = os.path.join(path, name)
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+    bing_storage = {'root_dir': file_path}
+    bing_crawler = BingImageCrawler(parser_threads=2, downloader_threads=4, storage=bing_storage)
+    bing_crawler.crawl(keyword=name, max_num=40)
+    print('第{}位明星：{}'.format(i, name))
