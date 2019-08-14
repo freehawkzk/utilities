f4ab1fb895c8edc62fdee6bdadf870bd55bb12f5
freehawkzk <freehawkzk@126.com>
2019-08-14 17:04:57 +0800

    0.1.5.14 从Matting_Human_Half数据集中分离出alpha通道

---
+++ extraceAlphaFromPng.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import cv2
+from tools.mytools import ReadImgFileList
+
+imglistfilepath=sys.argv[1]
+
+imgs=ReadImgFileList(imglistfilepath)
+
+for file in imgs:
+    f = os.path.abspath(file)
+    f=f.replace("\\",'/')
+    s=f.split('/')
+    s[3]="alpha"
+    dir = s[0:-1]
+    d=s[0]+'/'
+    for do in dir[1:]:
+        d=d+do+'/'
+    if not os.path.exists(d):
+        os.makedirs(d)
+    img = cv2.imread(file,cv2.IMREAD_UNCHANGED)
+    b,g,r,alpha = cv2.split(img)
+    cv2.imwrite(d+s[-1],alpha)
+    print(file)
+
+    
